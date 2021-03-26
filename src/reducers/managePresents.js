6edadfaf4c4bd32f5642f88c0b1b5fad9a8cebd2
Matthew Crooks <mfcrooks@gmail.com>
2021-03-26 20:29:57 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,5 +1,13 @@
-export function managePresents(state, action){
-  action = {
-    type: "INCREASE"
+export function managePresents(state= {
+  numberOfPresents: 0,
+}, action){
+  switch (action.type) {
+    case INCREASE:
+      return Object.assign({}, state, {
+        numberOfPresents: state.numberOfPresents + 1
+      });
+
+    default:
+      return satte;
   }
 }
